0fd7c78dd9878c642c90c8df333d78a72c6ea3b0
Alex Lee <hyunho98@gmail.com>
2023-03-02 18:13:08 -0500

    commit tree 9f449c67be0940e1fa5dfcb8c11d43f48bf47e45

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [filteredListings, setFilteredListings] = useState(listings)
+
+  function handleSearchSubmit(filterValue) {
+    if (filterValue === "") {
+      setFilteredListings(listings)
+    } else {
+      setFilteredListings(listings.filter((listing) => (listing.description.includes(filterValue) || listing.location.includes(filterValue))))
+    }
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearchSubmit={handleSearchSubmit}/>
+      <ListingsContainer listings={filteredListings} setListings={setListings}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,34 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, setListings }) {
+  useEffect(() => {
+    fetch(`http://localhost:6001/listings`)
+      .then((r) => r.json())
+      .then((data) => setListings(data))
+  }, [])
+
+  const listingCards = listings.map((listing) => {
+    return (
+      <ListingCard
+        key={listing.id}
+        listing={listing}
+        onDeleteListing={handleListingDelete}
+      />
+    )
+  })
+
+  function handleListingDelete(deletedListing) {
+    const updatedListings = 
+    listings.filter((listing) => listing.id !== deletedListing.id)
+
+    setListings(updatedListings)
+  }
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
